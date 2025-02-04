29bd2e01dd313c9742066c8e37f1f83defadae9b
Mehrdad Karami <mk24167@email.vccs.edu>
2025-02-03 20:40:41 -0500

    fix syntax issues

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { resolve } from "path";
 
 export default defineConfig({
     build: {
+        base: "/pages-site/",
+        outDir: "docs",
         rollupOptions: {
-            base: "/pages-site/",
-            outDir: "docs",
             input: {
                 main: resolve(__dirname, "index.html"),
-                about: resolve(__dirname, "./about/index.html"),
-                information: resolve(__dirname, "./information/index.html"),
+                about: resolve(__dirname, "about/index.html"),
+                information: resolve(__dirname, "information/index.html"),
             },
         },
     },
