e0fb6fa9ef524bf441641f65c9c49d6ba193b6ae
Mehrdad Karami <mk24167@email.vccs.edu>
2025-02-10 12:51:09 -0500

    Adding React

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     base: "/pages-site/",
     build: {
-        outDir: "../docs",
+        outDir: "docs",
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
